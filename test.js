b60d807638999f69b2e47daf6c6648deb831f22e
Psyduk <thestryka@gmail.com>
2013-05-14 15:17:46 -0700

    Formatting

--- test.js
+++ test.js
@@ -2,6 +2,36 @@ var assert         = require('assert'),
     transliterator = require('./');
 
 describe('transliterator', function () {
+  it('should return null when passing in null', function () {
+    var test;
+    try {
+      transliterator(null);
+    } catch (e) {
+      test = e;
+    }
+    assert.equal(test, null);
+  });
+
+  it('should return undefined when passing in undefined', function () {
+    var test;
+    try {
+      transliterator(undefined);
+    } catch (e) {
+      test = e;
+    }
+    assert.equal(test, undefined);
+  });
+
+  it('should throw an error when passing in non strings', function () {
+    var test;
+    try {
+      transliterator([]);
+    } catch (e) {
+      test = e;
+    }
+    assert.notEqual(test, undefined);
+  });
+
   it('should map each character key to the correct character(s) value', function () {
     Object.keys(transliterator.characterMap).forEach(function (val) {
       assert.equal(transliterator(val), transliterator.characterMap[val]);
@@ -30,35 +60,5 @@ describe('transliterator', function () {
     assert.equal(transliterator('14141ྠ', 1), '141411');
     assert.equal(transliterator('Ⓩbad ሏ', '_'), '_bad _');
     assert.equal(transliterator('պ պ պ պ', '_'), '_ _ _ _');
-  });
-
-  it('should throw an error when passing in non strings', function () {
-    var test;
-    try {
-      transliterator([]);
-    } catch (e) {
-      test = e;
-    }
-    assert.notEqual(test, undefined);
-  });
-
-  it('should return null when passing in null', function () {
-    var test;
-    try {
-      transliterator(null);
-    } catch (e) {
-      test = e;
-    }
-    assert.equal(test, null);
-  });
-
-  it('should return undefined when passing in undefined', function () {
-    var test;
-    try {
-      transliterator(undefined);
-    } catch (e) {
-      test = e;
-    }
-    assert.equal(test, undefined);
   });
 });
